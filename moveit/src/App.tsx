06e62af86a4c47532241ab779cd4d13e170bc4ea
unknown <ygorsallesaniceto@gmail.com>
2021-02-23 18:17:01 -0300

    Init front-end

---
+++ moveit/src/App.tsx
@@ -0,0 +1,12 @@
+import { ExperienceBar } from './components/ExpierenceBar';
+import './styles/global.css';
+
+function App() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
+
+export default App;
