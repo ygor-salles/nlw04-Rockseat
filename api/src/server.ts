25030c8d8d9fc0851ae486fe17da7fa572d8ad03
unknown <ygorsallesaniceto@gmail.com>
2021-02-23 19:03:05 -0300

    Finalizando aula 1 backend

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,4 +2,15 @@ import express from 'express';
 
 const app = express();
 
+app.get('/users', (req, res) => {
+    return res.json({ message: "Hellow world - NLW04" })
+});
+
+app.post('/', (req, res) => {
+    //recebeu os dados para salvar
+    return res.json({ message: "Os dados foram salvos com sucesso" })
+})
+
+
+
 app.listen(3333, () => console.log('Server is run'));
